0953a4ebca09aa5f8ec0099560beadf7109df329
LNatb <laura-natalia.beltran.1@ulaval.ca>
2024-03-05 10:21:35 -0500

    Human-like typing test

--- Exercice.py
+++ Exercice.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+import time
+import random
+import sys
+
+def fake_type(words):
+    words += "\n"
+    for char in words:
+        time.sleep(random.choice([
+          0.3, 0.11, 0.08, 0.07, 0.07,
+          0.07, 0.06, 0.06, 0.05, 0.01
+        ]))
+        sys.stdout.write(char)
+        sys.stdout.flush()
+    time.sleep(1)
+
+fake_type("Hello World! I am an imaginary being muahahah")
